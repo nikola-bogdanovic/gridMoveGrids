ff5e4f6648dba6c98cb40a30e0c6cf70389a897d
nikola-bogdanovic <nkla.bog@gmail.com>
2015-12-01 21:36:43 +0100

    commit

--- build.php
+++ build.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$numberOfMonitors = 2;                          // EDIT THIS ACCORDING TO YOUR NUMBER OF MONITORS
+	$numberOfMonitors = 1;                          // EDIT THIS ACCORDING TO YOUR NUMBER OF MONITORS
 	
 	$inputFile = fopen("inputFile.txt", "r");       // BEST LEAVE AS IS
 	$outputFile = fopen("inDevelopment.grid", "w"); // BEST LEAVE AS IS
@@ -51,3 +51,5 @@ NumberOfGroups = " . $counter . "\n";
 	
 	fclose($inputFile);
 	fclose($outputFile);
+	
+	echo "Finished building inDevelopment.grid";
